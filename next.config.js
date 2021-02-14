26aa964441d1ff387be1682a1f6bfdcda9384e04
Zak Hargreaves <ZakHargz@users.noreply.github.com>
2021-02-14 18:53:50 +0000

    huh, just work!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const optimizedImages = require('next-optimized-images');
+
 module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" }
     }
   },
+  optimizedImages()
 }
